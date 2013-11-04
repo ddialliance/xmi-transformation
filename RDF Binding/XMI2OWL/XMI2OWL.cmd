8406b6b6dafc22783c503d3852a9d68fdf019640
boschthomas <boschthomas@a2d62530-183e-410b-950b-468b89b80bf2>
2013-11-04 16:42:09 +0000

    XMI2OWL
    git-svn-id: https://subversion.assembla.com/svn/ddi-moving-forward@2 a2d62530-183e-410b-950b-468b89b80bf2

---
+++ RDF Binding/XMI2OWL/XMI2OWL.cmd
@@ -0,0 +1,3 @@
+java net.sf.saxon.Transform -s:XMI.xml -xsl:XMI2OWL.xsl -o:OWL.owl baseOntologyIRI=http://rdf-vocabulary.ddialliance.org/DDICoreVocabulary compositionName=contains
+	
+PAUSE transformation successful!
